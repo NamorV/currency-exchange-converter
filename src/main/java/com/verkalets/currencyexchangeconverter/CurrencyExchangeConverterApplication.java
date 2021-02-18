a317e9abcf4e2b8e2a80c9ef9a9eb0d00bd445e1
NamorV <drednout309@gmail.com>
2021-02-18 19:34:25 +0100

    initial commit

---
+++ src/main/java/com/verkalets/currencyexchangeconverter/CurrencyExchangeConverterApplication.java
@@ -0,0 +1,13 @@
+package com.verkalets.currencyexchangeconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/verkalets/currencyexchangeconverter/CurrencyExchangeConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verkalets.currencyexchangeconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
